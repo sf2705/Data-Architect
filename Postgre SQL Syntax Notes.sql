1803dc2be0b4c8ee95bddbd292a103bbd9800407
Siqi Feng <36309789+sf2705@users.noreply.github.com>
2021-04-04 18:30:34 -0400

    add: create stage

--- Postgre SQL Syntax Notes.sql
+++ Postgre SQL Syntax Notes.sql
@@ -56,6 +56,43 @@ Drop table Employee;
 
 
 
+------ Create stage 
+
+-- create stage table
+Create table Stg(player varchar(50), team varchar(50), position varchar(50));
+
+-- load data into stage table
+Insert into Stg Values ('Bob', 'Browns', 'Quarterback'),
+('Tyrell', 'Reds', 'Full Back'),
+('Darrin', 'Blues', 'Kicker'),
+('Frank', 'Reds', 'Quarterback');
+
+-- create table and load data into table from stage
+-- create a table
+Create table Team (
+team_id serial primary key,
+team_nm varchar(50));
+
+Create table Position (
+position_id serial primary key,
+position_nm varchar(50));
+
+Create table Player (
+player_id serial primary key,
+player_nm varchar(50),
+team_id int references Team(team_id),
+position_id int references Position(position_id));
+
+-- load data
+Insert into Team(team_nm) Select distinct team from Stg;
+
+Insert into Position(position_nm) Select distinct position from Stg;
+
+Insert into Player(player_nm, team_id, position_id) Select s.player, t.team_id, p.position_id
+From stg as s Join team as t On s.team = t.team_nm
+			  Join position as p On s.position = p.position_nm;
+
+
 
 
 
